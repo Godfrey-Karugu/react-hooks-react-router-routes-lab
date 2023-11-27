dafa6b4251caf929e73ea305f4e48e2d8cefe20c
Godfrey Karugu <karugugk@gmail.com>
2023-11-27 09:19:59 +0300

    completed the lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+    <h1> Movies</h1>
+    {
+     movies.map(({title,time,genres})=>{
+      return(<div key={title}>
+        <h3>{title}</h3>
+        <p>{time}</p>
+        <ul>{genres.map((genre,index)=>(<li key={index}>{genre}</li>))}</ul>
+      </div>)
+     }) 
+    }
+    
+    </div>
+  );
 }
 
 export default Movies;
